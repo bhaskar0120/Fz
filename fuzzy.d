b790370adbe902a755c0405b848f58965c96fee5
Bhaskar Bhardwaj <bhaskar0120@gmail.com>
2024-01-07 09:01:08 +0530

    Working on fzignore

--- fuzzy.d
+++ fuzzy.d
@@ -1,13 +1,55 @@
 import std.stdio:writeln, writef, File;
-import std.string:leftJustify;
+import std.string:leftJustify, splitLines;
+import std.file:dirEntries, SpanMode, read;
 
 float score(size_t node, const string src, const string dest){
   // weighted edit distance
   return 0.0;
 }
 
-const size_t OPTIONS = 5;
+enum Type{
+  ALL, SPECIFIC, SKIPPABLE, BOTH
+}
+
+bool[string] look,nolook;
+struct Spider{
+  string currDir;
+  string[] list;
+  size_t pos = 0;
+  Type type;
+
+  this(string DIR){
+    currDir = DIR;
+    if(look.length >= 0 && nolook.length >= 0)
+      type = Type.BOTH;
+    else if(look.length >= 0)
+      type = Type.SPECIFIC;
+    else if (nolook.length)
+      type = Type.SKIPPABLE;
+    else
+      type = Type.ALL;
+
+
+    foreach(file ;dirEntries(currDir, SpanMode.shallow, false)){
+      list ~= file;
+    }
+    writeln(list);
+  }
+
+  bool empty(){
+
+    return false;
+  }
+  string front(){
+    return "";
+  }
+  void popfront(){
+  }
+}
+
+
 // TUI FUNCTION
+const size_t OPTIONS = 5;
 
 void clean(){
   for(size_t t = 0; t < OPTIONS;++t) writeln();
@@ -33,6 +75,23 @@ void printLines(const string[] options, size_t highlight){
   }
 }
 
+// TUI ENDS
+
+
+void readAllowedFiles(ref bool[string] look,ref bool[string] nolook) {
+  auto lines = (cast(string)read(".fzignore")).splitLines();
+  foreach(line;lines){
+    if(line.length == 0) continue;
+    if(line[0] == '!'){
+      auto temp = cast(string)line[1..$];
+      look[temp] = true;
+    }
+    else{
+      nolook[line] = true;
+    }
+  }
+
+}
 
 
 
@@ -46,6 +105,13 @@ int main(){
     "Them",
     "Then"
   ];
+  auto files = dirEntries("/home/somi",SpanMode.shallow,false);
   printLines(arr,0);
+  writeln(files);
+  readAllowedFiles(look,nolook);
+  writeln(nolook.length,look.length);
+  auto sp = Spider("/home/somi");
+
   return 0;
+
 }
